922a4d023eb01f904264fd37d3ae31e495885d38
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: LiveDisplay driver
     * Provide a simple color temperature adjustment interface via MDSS PCC.
     * Provide hardware integration support for CABC, SRE, and CE.
    
    Change-Id: I4ed1f3876767dd6bedccaf0bbf61af7d36730ca0
    
    video: mdss: Add new features and improve LiveDisplay
    
     * Add support for auto contrast optimization.
     * Add preset support.
     * Presets are arbitrary sets of DSI commands (defined in the DT) which
       can be used for implementing device-specific modes.
     * This adds the "preset" and "num_presets" sysfs nodes.
     * The preset at position zero is always applied.
     * Completely reworked the way we send commands to the hardware. Now we
       create a single DSI command buffer and send the whole thing at once.
    
    Change-Id: I9b986593cf42b65a3360dae474d8318406b48204
    
    video: mdss: Cache LiveDisplay RGB settings when display off
    
     * We should avoid waking up the hardware if not necessary-
       but unfortunately even if we do, we might not get a valid
       result when querying for the current PCC settings.
     * Get rid of this code and keep track of the desired values
       ourselves, and reapply it during wakeup.
    
    Change-Id: I1d42842af0d044e241dcc5b9266be0db4f84800b
    
    video: mdss: Fix dual-DSI issue in LiveDisplay
    
     * The second controller won't have it's own framebuffer, so don't
       dereference a NULL pointer.
    
    Change-Id: I0d0c1475882eb65c8d5bc69c9ac3776b5d60339f
    
    video: mdss: Implement PCC combining for LiveDisplay
    
     * If we send calibration data via ioctl, we want to make sure
       it's preserved when applying RGB offsets via LiveDisplay.
     * Add support for combining the values, using the calibrated
       PCC value as a scaling factor.
    
    Change-Id: I33a5678a858f5e1c609709b4e7e901c792fd2590
    
    video: mdss: Simplify PCC combiner even more
    
     * We shouldn't return zero from this.
    
    CYNGNOS-460
    
    2015-12-27	Steve Kondik	video: mdss: Improve the LiveDisplay driver
    2016-05-06	Ethan Chen	video: mdss: livedisplay: Fix livedisplay MODE_RGB functionality
    2016-05-16	Scott Mertz	video: mdss: remove erroneous mutex_unlock
    2016-05-05	Zhao Wei Liew	mdss: livedisplay: Use CABC command chain for CE
    2017-06-19	Michael Bestas	mdss: livedisplay: Wrap to 80 chars per line and fix code style
    
    Change-Id: I181a878d442e55e930c1a9532c40c84c3a9b1361
    Signed-off-by: Akianonymus <anonymus.aki@gmail.com>

--- drivers/video/msm/mdss/mdss_panel.h
+++ drivers/video/msm/mdss/mdss_panel.h
@@ -425,6 +425,8 @@ struct mdss_mdp_pp_tear_check {
 	u32 refx100;
 };
 
+struct mdss_livedisplay_ctx;
+
 struct mdss_panel_info {
 	u32 xres;
 	u32 yres;
@@ -516,6 +518,8 @@ struct mdss_panel_info {
 	u32 bl_hbm_off;
 	u32 bl_hbm_on_max;
 	u32 bl_hbm_off_max;
+
+	struct mdss_livedisplay_ctx *livedisplay;
 };
 
 struct mdss_panel_data {
